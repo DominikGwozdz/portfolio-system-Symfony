b9e8de6510a4e0e56d5bef2c7b96a8e3a5813211
Dominik Gwozdz <dwgwozdz@gmail.com>
2020-01-03 22:25:18 +0100

    send description to view on about site

--- src/Controller/AboutController.php
+++ src/Controller/AboutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\About;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,11 @@ class AboutController extends AbstractController
      */
     public function index()
     {
+        //always get id = 1 because there is only 1 record in this table
+        $about = $this->getDoctrine()->getRepository(About::class)->find(1);
+
         return $this->render('about/index.html.twig', [
-            'controller_name' => 'AboutMeController',
+            'about' => $about
         ]);
     }
 }
