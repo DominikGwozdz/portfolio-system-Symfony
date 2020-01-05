16b7ec78e45d7012d7d616f36ad95672d3c22d0b
Dominik Gwozdz <dwgwozdz@gmail.com>
2020-01-05 14:18:37 +0100

    updated fields options

--- src/Form/AboutType.php
+++ src/Form/AboutType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\About;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,26 @@ class AboutType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('picture')
-            ->add('meta_description')
-            ->add('description')
+            ->add('picture',FileType::class,
+                [
+                    'label' => 'Zdjęcie',
+                    'required' => false
+                ])
+            ->add('meta_description', TextType::class,
+                [
+                    'label' => 'Opis dla wyszukiwarek (od 120 do 158 znaków)'
+                ])
+            ->add('description', TextareaType::class,
+                [
+                    'label' => 'Opis o mnie',
+                    'attr' => [
+                        'rows' => '10'
+                    ]
+                ])
+            ->add('submit', SubmitType::class,
+                [
+                    'label' => 'Zapisz'
+                ])
         ;
     }
 
