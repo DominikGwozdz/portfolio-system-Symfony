39692aa6f3009ae6cf2e146f7908d4c867efc6c4
Dominik Gwozdz <dwgwozdz@gmail.com>
2020-02-05 20:51:43 +0100

    update sql migration - should not update all fields when add only one

--- src/Migrations/Version20200205113909.php
+++ src/Migrations/Version20200205113909.php
@@ -23,10 +23,6 @@ final class Version20200205113909 extends AbstractMigration
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
         $this->addSql('ALTER TABLE gallery ADD slug VARCHAR(500) DEFAULT NULL UNIQUE AFTER name, CHANGE category_id category_id INT DEFAULT NULL, CHANGE picture picture VARCHAR(500) DEFAULT NULL, CHANGE password password VARCHAR(500) DEFAULT NULL');
-        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
-        $this->addSql('ALTER TABLE gallery_category CHANGE picture picture VARCHAR(500) DEFAULT NULL');
-        $this->addSql('ALTER TABLE about CHANGE picture picture VARCHAR(255) DEFAULT NULL, CHANGE meta_description meta_description VARCHAR(255) DEFAULT NULL');
-        $this->addSql('ALTER TABLE gallery_item CHANGE gallery_id gallery_id INT DEFAULT NULL');
     }
 
     public function down(Schema $schema) : void
@@ -34,10 +30,6 @@ final class Version20200205113909 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE about CHANGE picture picture VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE meta_description meta_description VARCHAR(255) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE gallery DROP slug, CHANGE category_id category_id INT DEFAULT NULL, CHANGE picture picture VARCHAR(500) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`, CHANGE password password VARCHAR(500) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE gallery_category CHANGE picture picture VARCHAR(500) CHARACTER SET utf8mb4 DEFAULT \'NULL\' COLLATE `utf8mb4_unicode_ci`');
-        $this->addSql('ALTER TABLE gallery_item CHANGE gallery_id gallery_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT CHARACTER SET utf8mb4 NOT NULL COLLATE `utf8mb4_bin`');
+        $this->addSql('ALTER TABLE gallery DROP slug');
     }
 }
