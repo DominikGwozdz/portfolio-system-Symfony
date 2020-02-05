cb4d07da9838253c61ee81bfef714971652d6a5a
Dominik Gwozdz <dwgwozdz@gmail.com>
2020-02-05 21:07:19 +0100

    add migration for user and update entity - add first name and last name fields

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -24,6 +24,30 @@ class User implements UserInterface
      */
     private $email;
 
+    public function getFirstName(): ?string
+    {
+        return $this->first_name;
+    }
+
+    public function setFirstName(string $first_name): self
+    {
+        $this->first_name = $first_name;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->last_name;
+    }
+
+    public function setLastName(string $last_name): self
+    {
+        $this->last_name = $last_name;
+
+        return $this;
+    }
+
     /**
      * @ORM\Column(type="json")
      */
@@ -34,6 +58,16 @@ class User implements UserInterface
      * @ORM\Column(type="string")
      */
     private $password;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $first_name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $last_name;
 
     public function getId(): ?int
     {
