ca30face70ce27d2123daf4ad70f0e7d39701b04
Dominik Gwozdz <dwgwozdz@gmail.com>
2020-02-05 14:15:23 +0100

    send category to view

--- src/Controller/GalleryController.php
+++ src/Controller/GalleryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\GalleryCategory;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,8 +13,11 @@ class GalleryController extends AbstractController
      */
     public function index()
     {
+        $em = $this->getDoctrine()->getManager();
+        $categories = $em->getRepository(GalleryCategory::class)->findBy(['is_visible' => '1']);
         return $this->render('gallery/index.html.twig', [
             'controller_name' => 'GalleryController',
+            'categories' => $categories,
         ]);
     }
 }
